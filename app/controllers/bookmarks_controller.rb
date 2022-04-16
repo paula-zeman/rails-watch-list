f29121a1cece81da3777a10b6a4ae5abac742a2b
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-04-16 17:55:07 +0200

    added bookmark routes

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,24 @@
 class BookmarksController < ApplicationController
+  def new
+  @list = List.find(params[:list_id])
+  @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    @list.save
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "lists", to: "lists#index"
-  get "lists/new", to: "lists#new"
-  get "lists/:id", to: "lists#show", as: :list
-  post "lists", to: "lists#create"
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new"
+  # get "lists/:id", to: "lists#show", as: :list
+  # post "lists", to: "lists#create"
 
+  # get "bookmarks/new", to: "bookmarks#new"
+  # post "bookmarks", to: "bookmarks#create"
+  resources :lists, only: [:index, :new, :show, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create, :show]
+  end
+  resources :bookmarks, only: [:destroy]
 end
