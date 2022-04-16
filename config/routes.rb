93074b04db3120ed349b9c0ee84c926333c0ff32
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-04-16 16:29:20 +0200

    built list controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "lists", to: "lists#index"
+  get "lists/new", to: "lists#new"
+  get "lists/:id", to: "lists#show", as: :list
+  post "lists", to: "lists#create"
+
 end
