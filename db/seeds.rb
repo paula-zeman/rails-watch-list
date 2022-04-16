802c43bc15cdf482fccdd225c05003c9e7a2ae6d
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-04-16 14:52:20 +0200

    db seed movies faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning up database"
+Movie.destroy_all
+puts "Database clean"
+
+30.times do
+  Movie.create(
+    title: Faker::Book.title,
+    overview: Faker::Hipster.sentence,
+    poster_url: Faker::Internet.url,
+    rating: rand(1..5)
+  )
+end
+
+puts "Done"
